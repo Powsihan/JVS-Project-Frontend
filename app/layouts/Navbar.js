d78da0cbb7919d3bae9e8e0d231fe853468e1d39
Powsihan <powsipowsihan07@gmail.com>
2024-05-09 14:54:37 +0530

    Navbar chnages done

--- app/layouts/Navbar.js
+++ app/layouts/Navbar.js
@@ -1,11 +1,13 @@
-
 import Image from "next/image";
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/navbar.css";
 import logo from "../assets/images/Logo.png";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import notification from "../assets/icons/bell.svg";
-import avatar from "../assets/images/Avatar.png"
+import avatar from "../assets/images/Avatar.png";
+import CommonButton from "../components/CommonButton";
+import Contact from "../assets/icons/Headset.png";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,119 +15,141 @@ const Navbar = () => {
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
-    // <div
-    //   className="navbar w-100"
-    //   style={{ height: "100px", backgroundColor: "lightblue" }}
-    // >
-    //   <div>
-    //     <Image src={logo} alt="" />
-    //   </div>
-    //   <div>
-    //     <div className="Navbar-box">
-    //       Home
-    //     </div>
-    //     <div></div>
-    //     <div></div>
-    //     <div></div>
-    //     <div></div>
-    //   </div>
-    //   <div></div>
-    // </div>
-    <nav className="navbar navbar-expand-lg w-100 Navbar-Container">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <MenuIcon />
-        </button>
+    <>
+      <nav
+        className={`navbar navbar-expand-lg w-100 Navbar-Container ${
+          menuOpen ? "open" : "close"
+        }`}
+      >
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={toggleMenu}
+          >
+            {menuOpen ? <CloseIcon /> : <MenuIcon />}
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <a className="navbar-brand mt-2 mt-lg-0" href="#">
-            <Image src={logo} alt="" />
-          </a>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="#home">
-                <div className="Navbar-box">Home</div>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#AboutUS">
-                <div className="Navbar-box">About US</div>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#SellVehicles">
-                <div className="Navbar-box">Sell Your Vehicle</div>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#Customization">
-                <div className="Navbar-box">Customization</div>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#Auction">
-                <div className="Navbar-box">Auction</div>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="d-flex align-items-center">
-          <div className="dropdown">
-            <a
-              className="link-secondary me-3"
-              href="#"
-              id="navbarDropdownMenuLink"
-              role="button"
-              aria-expanded="false"
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div
+              className={`d-flex align-items-center w-100 pe-5 ps-5 ${
+                windowSize[0] >= 800 ? "d-flex flex-row" : "d-flex flex-column"
+              }`}
             >
-              <Image src={notification} alt="" width={25}/>
-            </a>
+              <a
+                className="navbar-brand mt-2 mt-lg-0 d-flex align-items-center"
+                href="#"
+              >
+                <Image src={logo} alt="" />
+              </a>
+              <ul
+                className="navbar-nav me-auto  mb-lg-0 d-flex justify-content-center gap-2 flex-grow-1 w-100"
+              >
+                <li className="nav-item">
+                  <a className="nav-link" href="#home">
+                    <div className="Navbar-box">Home</div>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#AboutUS">
+                    <div className="Navbar-box">About US</div>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#SellVehicles">
+                    <div className="Navbar-box">Sell Your Vehicle</div>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#Customization">
+                    <div className="Navbar-box">Customization</div>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#Auction">
+                    <div className="Navbar-box">Auction</div>
+                  </a>
+                </li>
+              </ul>
+              <div
+                className="d-flex align-items-center"
+              >
+                <CommonButton text={"Contact"} image={Contact} width={110}/>
+              </div>
+            </div>
           </div>
+          <div className="d-flex align-items-center">
+            <div className="dropdown">
+              <a
+                className="link-secondary me-3"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                aria-expanded="false"
+              >
+                <Image src={notification} alt="" width={25} />
+              </a>
+            </div>
 
-          <div className="dropdown">
-            <a
-              className="dropdown-toggle d-flex align-items-center hidden-arrow"
-              href="#"
-              id="navbarDropdownMenuAvatar"
-              role="button"
-              aria-expanded="false"
-            >
-              <Image src={avatar} alt=""/>
-            </a>
-            <ul
-              className="dropdown-menu dropdown-menu-end"
-              aria-labelledby="navbarDropdownMenuAvatar"
-            >
-              <li>
-                <a className="dropdown-item" href="#">
-                  My profile
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Settings
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Logout
-                </a>
-              </li>
-            </ul>
+            <div className="dropdown">
+              <a
+                className="dropdown-toggle d-flex align-items-center hidden-arrow"
+                href="#"
+                id="navbarDropdownMenuAvatar"
+                role="button"
+                aria-expanded="false"
+              >
+                <Image src={avatar} alt="" />
+              </a>
+              <ul
+                className="dropdown-menu dropdown-menu-end"
+                aria-labelledby="navbarDropdownMenuAvatar"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    My profile
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 "use client"
 import CommonButton from "./components/CommonButton";
-import Contact from "./assets/icons/Headset.png";
+
 import Navbar from "./layouts/Navbar";
 
 export default function Home() {
   return (
     <main className="d-flex flex-column align-items-center w-100 justify-content-center min-vh-100">
       <h1 className="text-success">Heloo Project</h1>
-      <CommonButton text={"Login"}  width={110}/>
       <Navbar/>
     </main>
   );
