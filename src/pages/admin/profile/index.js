aad5900863d68e710ab350f3193315a5ceef3649
Powsihan <powsipowsihan07@gmail.com>
2024-05-13 21:44:50 +0530

    Profile Section Alignment Chnages

--- src/pages/admin/profile/index.js
+++ src/pages/admin/profile/index.js
@@ -1,90 +1,92 @@
-import Adminlayout from '@/src/layouts/Adminlayout';
-import React from 'react';
+import Adminlayout from "@/src/layouts/Adminlayout";
+import React from "react";
 import CommonButton from "@/src/components/CommonButton";
 import Image from "next/image";
-import avatar from "/Applications/Project02/JVS-Project-Frontend/src/assets/images/Avatar.png";
-import editprof from "/Applications/Project02/JVS-Project-Frontend/src/assets/images/Editprof.png";
-import changepass from "/Applications/Project02/JVS-Project-Frontend/src/assets/images/Changepass.png";
+import avatar from "../../../assets/images/avatar.svg";
+import editprof from "../../../assets/images/Editprof.png";
+import changepass from "../../../assets/images/Changepass.png";
 import "../../../styles/admin.css";
 
 import "./profile.css";
 
 import TextField from "@/src/components/TextField";
+import { Button } from "react-bootstrap";
 
 const index = () => {
   return (
     <Adminlayout>
-      <div className="container-fluid h-100 position-relative">
-        <div className="row h-100">
-          <div className="col-sm-4 d-flex flex-column justify-content-start align-items-center" style={{ backgroundColor: 'white' }}>
-            <div style={{ width: '150px', height: '150px', borderRadius: '50%', overflow: 'hidden', backgroundColor: 'lightgray', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
-              <div>
-                <Image src={avatar} className="avatar" alt="avatar" style={{ objectFit: 'cover', width: '150px', height: '150px', borderRadius: '50%' }} />
-              </div>
+      <div className="container-fluid Profile-Edit-Section">
+        <div className="row p-1">
+          <div className="col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-center Profile-Edit-Section p-2">
+            <div className="d-flex alig-items-center justify-content-center">
+              <Image src={avatar} className="avatar" alt="avatar" width={150} />
             </div>
-            <div className="mt-2 text-center">
-              <div>Ratheeswaran</div>
-              <div>Admin</div>
+            <hr />
+            <div className="text-center Profile-name-section pb-3">
+              <h1>Ratheeswaran</h1>
+              <h2>Admin</h2>
             </div>
-            <div>
-              <CommonButton text="Edit Profile" width="150px" onPress={() => console.log("Edit Profile button pressed")} />
+            <div className="ps-3 pe-3">
+              <CommonButton text="Edit Profile" width={"100%"} />
             </div>
-            <div className="col-sm-8 d-flex justify-content-center align-items-center" style={{ position: 'relative' }}>
-              <Image src={editprof} alt="Edit Profile" style={{ width: '227px', height: '227px', zIndex: '1' }} />
+            <div className="d-flex justify-content-center align-items-center">
+              <Image src={editprof} alt="Edit Profile" width={350} />
             </div>
           </div>
-          <div className="col-sm-8">
-            <div className="col-sm-12 border-bottom custom-border" style={{ backgroundColor: 'white', height: '50%', padding: '10px' }}>
-              <span className="border-start custom-border" style={{ borderLeftWidth: '4px' }}>Profile Edit</span>
-              <div className="row">
-                <div className="col-sm-6 mb-3">
+          <div className="col-lg-8 col-md-6 col-sm-12">
+            <div className="col-sm-12 Profile-Edit-Section">
+              <h5 className="custom-border ps-2">Profile Edit</h5>
+              <div className="row p-2">
+                <div className="col-lg-6 col-sm-12 mb-3">
                   <TextField label={"Name"} placeholder={"Name"} />
                 </div>
-                <div className="col-sm-6 mb-3">
+                <div className="col-lg-6 col-sm-12 mb-3">
                   <TextField label={"Email"} placeholder={"email"} />
                 </div>
-                <div className="col-sm-6 mb-3">
+                <div className="col-lg-6 col-sm-12 mb-3">
                   <TextField label={"Phone No"} placeholder={"phone no"} />
                 </div>
-                <div className="col-sm-6 mb-3">
-                  <div className="col-sm-6 mb-3">
-                    <TextField label="Choose Profile Picture" placeholder="Choose Profile Picture" type="file" id="profilePicture" />
-                  </div>
-                  <div className="row mt-3">
-                    <div className="col-sm-6 mb-3">
-                      <CommonButton text="Save Changes" width="100%" onPress={() => console.log("Save Changes button pressed")} />
-                    </div>
-                    <div className="col-sm-6 mb-3">
-                      <CommonButton text="Cancel" width="60%" onPress={() => console.log("Cancel button pressed")} />
-                    </div>
-                  </div>
+                <div className="col-lg-6 col-sm-12 mb-3">
+                  <TextField
+                    label="Choose Profile Picture"
+                    placeholder="Choose Profile Picture"
+                    type="file"
+                    id="profilePicture"
+                  />
                 </div>
+              </div>
+              <hr />
+              <div className="d-flex flex-row justify-content-end gap-3 mb-3 pe-2">
+                <CommonButton text="Save Changes" />
+                <Button variant="secondary">Cancel</Button>
               </div>
             </div>
-            <div className="col-sm-12 border-bottom custom-border" style={{ backgroundColor: 'white', height: '50%', padding: '10px' }}>
-              <span className="border-start custom-border" style={{ borderLeftWidth: '4px' }}>Password Change</span>
-              <div className="row">
-                <div className="col-sm-8 mb-3">
-                  <TextField label={"Current Password"} placeholder={"current password"} />
-                </div>
-                <div className="col-sm-8 mb-3">
-                  <TextField label={"New Password"} placeholder={"new password"} />
+            <div className="col-lg-12 Profile-Edit-Section mt-2">
+              <h5 className="custom-border ps-2">Password Change</h5>
 
+              <div className="row p-2">
+                <div className="col-lg-7 d-flex flex-column gap-2">
+                  <TextField
+                    label={"Current Password"}
+                    placeholder={"current password"}
+                  />
+                  <TextField
+                    label={"New Password"}
+                    placeholder={"new password"}
+                  />
+                  <TextField
+                    label={"Confirm Password"}
+                    placeholder={"confirm password"}
+                  />
                 </div>
-                <div className="col-sm-8 mb-3">
-                  <TextField label={"Confirm Password"} placeholder={"confirm password"} />
-                </div>
-                <div className="col-sm-4 d-flex justify-content-center align-items-center">
-                  <Image src={changepass} alt="Password" style={{ width: '227px', height: '227px', zIndex: '1' }} />
+                <div className="col-lg-5 d-flex justify-content-center align-items-center">
+                  <Image src={changepass} alt="Password" />
                 </div>
               </div>
-              <div className="row mt-3">
-                <div className="col-sm-6 mb-3">
-                  <CommonButton text="Save Changes" width="100%" onPress={() => console.log("Save Changes button pressed")} />
-                </div>
-                <div className="col-sm-6 mb-3">
-                  <CommonButton text="Cancel" width="60%" onPress={() => console.log("Cancel button pressed")} />
-                </div>
+              <hr />
+              <div className="d-flex flex-row justify-content-end gap-3 mb-3 pe-2">
+                <CommonButton text="Save Changes" />
+                <Button variant="secondary">Cancel</Button>
               </div>
             </div>
           </div>
