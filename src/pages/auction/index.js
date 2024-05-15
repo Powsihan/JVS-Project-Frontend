9b2c20aef2a7c5bbd8251a03398f1b79aa94006b
Powsihan <powsipowsihan07@gmail.com>
2024-05-15 15:28:37 +0530

    module chnaged

--- src/pages/auction/index.js
+++ src/pages/auction/index.js
@@ -1,26 +1,13 @@
 import Navbar from "@/src/layouts/Navbar";
 import React from "react";
-import { buildUrl } from 'cloudinary-build-url';
-import Image from "next/image";
+
 
 const index = () => {
-  const url = buildUrl('samples/powsi/1_ac11zf', {
-    cloud: {
-      cloudName: 'dkvtkwars',
-    },
-  });
+ 
  
   return (
     <div>
-      {/* <Navbar /> */}
-      <Image
-              src={url}
-              alt="Galaxy"
-              width={500}
-              height={250}
-              // fill={true}
-            />
-
+      fdgfd
     </div>
   );
 };
