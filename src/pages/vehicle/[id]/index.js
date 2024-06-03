a5672d10129b020350aadcd64a72c482c28c08ad
Powsihan <powsipowsihan07@gmail.com>
2024-06-03 21:45:19 +0530

    Vehicle Details Page Done

--- src/pages/vehicle/[id]/index.js
+++ src/pages/vehicle/[id]/index.js
@@ -3,33 +3,209 @@ import { useRouter } from "next/router";
 import { getVehicleInfo } from "@/src/redux/action/vehicle"; // Assuming you have this function
 import { useDispatch } from "react-redux";
 import { setLoading } from "@/src/redux/reducer/loaderSlice";
+import Navbar from "@/src/layouts/Navbar";
+
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../../../styles/vehicle.css";
+
+import vehicleCardicon1 from "../../../assets/icons/Vehicle-Card-icon-1.svg";
+import vehicleCardicon2 from "../../../assets/icons/Vehicle-Card-icon-2.svg";
+import vehicleCardicon3 from "../../../assets/icons/Vehicle-Card-icon-3.svg";
+import vehicleCardicon4 from "../../../assets/icons/Vehicle-Card-icon-4.svg";
+import vehicleCardicon5 from "../../../assets/icons/Vehicle-Card-icon-5.svg";
+import downicon from "../../../assets/icons/downicon.svg";
+import upicon from "../../../assets/icons/upicon.svg";
+
+import Image from "next/image";
+import CommonButton from "@/src/components/CommonButton";
 
 const VehicleDetail = () => {
   const dispatch = useDispatch();
   const router = useRouter();
   const { id } = router.query;
-  const [vehicle, setVehicle] = useState(null);
+  const [vehicleData, setVehicleData] = useState(null);
+  const [showAllDetails, setShowAllDetails] = useState(false);
+  const [activeSection, setActiveSection] = useState("details");
 
   useEffect(() => {
     if (id) {
       dispatch(setLoading(true));
       getVehicleInfo(id, (res) => {
         if (res && res.data) {
-          setVehicle(res.data);
+          setVehicleData(res.data);
           dispatch(setLoading(false));
         } else {
-            dispatch(setLoading(false));
+          dispatch(setLoading(false));
           console.error("Error fetching vehicle details", res);
         }
       });
     }
   }, [id]);
- 
+
+  if (vehicleData) {
+    var vehicleshortDetails = [
+      {
+        icon: vehicleCardicon1,
+        name:
+          vehicleData.ownership && vehicleData.ownership === 1
+            ? "Brand-New"
+            : "Pre-Owned",
+      },
+      { icon: vehicleCardicon2, name: vehicleData.yom },
+      { icon: vehicleCardicon3, name: vehicleData.fuel },
+      { icon: vehicleCardicon4, name: vehicleData.color },
+      { icon: vehicleCardicon5, name: `${vehicleData.power} CC` },
+    ];
+  }
+
+  if (vehicleData) {
+    var vehicleDetails = [
+      { label: "Registration No", content: vehicleData.registerno },
+      { label: "Vehicle Name", content: vehicleData.name },
+      { label: "Vehicle Type", content: vehicleData.type },
+      { label: "Vehicle Brand", content: vehicleData.brand },
+      { label: "Vehicle Model", content: vehicleData.model },
+      { label: "Vehicle Color", content: vehicleData.color },
+      { label: "Vehicle Model Year", content: vehicleData.yom },
+      { label: "Vehicle Ownership", content: vehicleData.ownership },
+      { label: "GearBox", content: vehicleData.gear },
+      { label: "Fuel Type", content: vehicleData.fuel },
+      { label: "Fuel Capacity", content: vehicleData.fuelcap },
+      { label: "Mileage", content: vehicleData.mileage },
+      { label: "No of Doors", content: vehicleData.noofdoors },
+      { label: "No Of Seats", content: vehicleData.noofseats },
+    ];
+  }
+
+  const handleToggleDetails = () => {
+    setShowAllDetails(!showAllDetails);
+  };
+
+  const handleSectionChange = (section) => {
+    setActiveSection(section);
+  };
 
   return (
     <div>
-      <h1>{vehicle && vehicle.name}</h1>
-      {/* Render vehicle details here */}
+      <Navbar />
+      {vehicleData && (
+        <div className="container-fluid min-vh-100 p-5">
+          <div className="row" style={{ paddingTop: "120px" }}>
+            <div className="col-lg-6 col-md-6 col-sm-12">
+              {vehicleData && vehicleData.image && (
+                <Carousel showThumbs={true} autoPlay={true} infiniteLoop={true}>
+                  {vehicleData.image.map((image, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        position: "relative",
+                        width: "100%",
+                        height: "350px",
+                      }}
+                    >
+                      <img
+                        src={image}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "fill",
+                        }}
+                      />
+                    </div>
+                  ))}
+                </Carousel>
+              )}
+            </div>
+            <div
+              className="col-lg-6 col-md-6 col-sm-12 Vehicle-Detail-Container p-3"
+              // style={{ backgroundColor: "red" }}
+            >
+              <div
+                className="d-flex flex-row justify-content-between align-items-center mb-2"
+                // style={{ backgroundColor: "yellow" }}
+              >
+                <h1>{vehicleData.name}</h1>
+                <h4>{`LKR ${vehicleData.price}/-`}</h4>
+              </div>
+              <div className="d-flex justify-content-around pt-2 align-items-center ps-1 pe-1">
+                {vehicleshortDetails.map((content, index) => (
+                  <div
+                    className="d-flex flex-column align-items-center justify-content-center"
+                    key={index}
+                  >
+                    <div className="Vehicle-card-display-icon p-3">
+                      <Image src={content.icon} />
+                    </div>
+                    <h6 className="pt-1">{content.name}</h6>
+                  </div>
+                ))}
+              </div>
+              <hr />
+              <div className="d-flex flex-row justify-content-around align-items-center mt-3">
+                <CommonButton text={"Purchase"} width={250} />
+                <CommonButton text={"Make an Inquiry"} width={300} />
+              </div>
+              <hr />
+              <div className="row details-feature-section">
+                <div
+                  className={`col-6 d-flex justify-content-center p-2 details-feature-section-heading ${
+                    activeSection === "details" ? "active-section" : ""
+                  }`}
+                  onClick={() => handleSectionChange("details")}
+                >
+                  Vehicle Details
+                </div>
+                <div
+                  className={`col-6 d-flex justify-content-center p-2 details-feature-section-heading ${
+                    activeSection === "features" ? "active-section" : ""
+                  }`}
+                  onClick={() => handleSectionChange("features")}
+                >
+                  Features
+                </div>
+              </div>
+              <hr/>
+              {activeSection === "details" ? (
+                <div className="container-fluid ps-5 pe-5 pt-3">
+                  {(showAllDetails
+                    ? vehicleDetails
+                    : vehicleDetails.slice(0, 5)
+                  ).map((vehicle, index) => (
+                    <div className="d-flex align-items-center justify-content-between vehicle-detail-div p-2 ps-3 pe-3 mb-3">
+                      <h3>{vehicle.label}</h3>
+                      <h6>{vehicle.content}</h6>
+                    </div>
+                  ))}
+                  <div className="pt-3">
+                    <CommonButton
+                      text={showAllDetails ? "Show Less" : "Show More"}
+                      width={"100%"}
+                      onClick={handleToggleDetails}
+                      image={showAllDetails ? upicon : downicon}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <div className="container-fluid ps-5 pe-5 pt-3">
+                  <div className="row pt-2 ps-1 pe-1">
+                    {vehicleData.features.map((content, index) => (
+                      <div className="col mb-3" key={index}>
+                        <div
+                          className="Vehicle-card-display-icon p-3 d-flex justify-content-center align-items-center"
+                          style={{ width: "170px" }}
+                        >
+                          <h2>{content}</h2>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
